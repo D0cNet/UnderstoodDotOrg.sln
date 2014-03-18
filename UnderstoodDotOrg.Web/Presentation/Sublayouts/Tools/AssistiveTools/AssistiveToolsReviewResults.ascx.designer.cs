75b3d7c51aa34335db1a0a35da6eadede08d44cb
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-03-18 16:04:25 +0000

    added SC placeholder and removed search box HTML
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@63 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/AssistiveToolsReviewResults.ascx.designer.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/AssistiveToolsReviewResults.ascx.designer.cs
@@ -11,5 +11,14 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.AssistiveTools {
     
     
     public partial class AssistiveToolsReviewResults {
+        
+        /// <summary>
+        /// Placeholder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Sitecore.Web.UI.WebControls.Placeholder Placeholder1;
     }
 }
