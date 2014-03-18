643b83cd902b4fb370d4e2ed78792b60bb31a689
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-03-18 14:28:26 +0000

    added new sublayout for Assisitive Tools Search
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@55 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/Assisitive Tools Search.ascx
@@ -0,0 +1,1 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Assisitive Tools Search.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.AssistiveTools.Assisitive_Tools_Search" %>
---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/Assisitive Tools Search.ascx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.AssistiveTools
+{
+    public partial class Assisitive_Tools_Search : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
