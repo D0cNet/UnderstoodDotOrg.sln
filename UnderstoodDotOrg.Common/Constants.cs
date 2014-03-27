2b46ef5767f84bca897e2a31c56db6d94b463a35
jrathore <jrathore@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-03-27 07:51:31 +0000

    Extension and Common methods
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@91 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Common {
+   public class Constants {
+       public static readonly string SCLANG_QUERY_STRING = "sc_lang";
+    }
+}
