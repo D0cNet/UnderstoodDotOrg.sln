f298943559799333cb47d7504fa6e21e1f36a120
bgarnier <bgarnier@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-03-31 20:27:50 +0000

    Updated domain models that are for content items, users, children, and personalization.
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@100 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Understood/Common/IssueModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.Understood.Common
+{
+    class IssueModel
+    {
+        /// <summary>
+        /// The Sitecore GUID used for this Issue
+        /// </summary>
+        public string IssueID { get; set; }
+
+        /// <summary>
+        /// The User Friendly name for this Issue
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// The order that this issue should appear in when displayed
+        /// </summary>
+        public int DisplayOrder { get; set; }
+
+    }
+}
---
+++ UnderstoodDotOrg.Domain/Understood/Users/ChildModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Domain.Understood.Common;
+
+namespace UnderstoodDotOrg.Domain.Users
+{
+    class ChildModel
+    {
+        /// <summary>
+        /// The Sitecore GUID of the Grade that this child is in
+        /// </summary>
+        public string GradeID  { get; set; }
+
+        /// <summary>
+        /// A list of all of the Issues that this child is tagged with
+        /// </summary>
+        public List<IssueModel> Issues { get; set; }
+
+    }
+}
