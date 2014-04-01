c458f04583c117b9c5174343f9ad1f4afc70c600
dcouto <dcouto@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-01 21:07:36 +0000

    added the CURRENT_INDEX_NAME property.
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@109 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Common/Constants.cs
+++ UnderstoodDotOrg.Common/Constants.cs
@@ -7,5 +7,13 @@ using System.Threading.Tasks;
 namespace UnderstoodDotOrg.Common {
    public class Constants {
        public static readonly string SCLANG_QUERY_STRING = "sc_lang";
+
+	   public static string CURRENT_INDEX_NAME
+	   {
+		   get
+		   {
+			   return string.Format("sitecore_{0}_index", Sitecore.Context.Database.Name.ToLower());
+		   }
+	   }
     }
 }
