c9b9e1507e761da5417afe875e8fa35ee3ac44a1
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-02 19:47:23 +0000

    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@120 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Membership/Models/MemberInterests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.Membership.Models
+{
+    class MemberInterests
+    {
+        public MemberInterests()
+        {
+        }
+
+        public virtual List<Guid> SchoolIssues { get; set; }
+        public virtual List<Guid> WaysToHelp { get; set; }
+        public virtual List<Guid> HomeLife { get; set; }
+        public virtual List<Guid> GrowingUp { get; set; }
+        public virtual List<Guid> SocialEmotionalIssues { get; set; }
+        public virtual List<Guid> JourneyStatus { get; set; }
+
+    }
+}
