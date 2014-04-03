ceed13f4596e27e08791c58463c8739462dadb57
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-03 14:25:02 +0000

    finalized models for membership
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@122 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Domain/Membership/Models/Member.cs
+++ UnderstoodDotOrg.Domain/Membership/Models/Member.cs
@@ -8,5 +8,30 @@ namespace UnderstoodDotOrg.Domain.Membership.Models
 {
     class Member
     {
+        public Member()
+        {
+
+        }
+
+        public Guid MemberID { get; set; }
+        public string ScreenName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid PersonalityType { get; set; }
+        public string ZipCode { get; set; }
+
+
+        public bool allowConnections { get; set; }
+        public bool allowNewsletter { get; set; }
+        public bool isPrivate { get; set; }
+        public bool hasOtherChildren { get; set; }
+
+        public Guid HomeLife { get; set; }
+        public Guid Role { get; set; }
+
+        public virtual List<Child> Children { get; set; }
+        public virtual MemberInterests Interests { get; set; }
+
+
     }
 }
