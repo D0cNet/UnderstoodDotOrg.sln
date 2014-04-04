1fd9039deedf2928da12b072d1d98b828087d092
ybharote <ybharote@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-04 17:06:44 +0000

    Commit updated Author item code
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@125 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Domain/SitecoreCIG/Poses/BaseTemplate/Article/AuthorItem.instance.cs
+++ UnderstoodDotOrg.Domain/SitecoreCIG/Poses/BaseTemplate/Article/AuthorItem.instance.cs
@@ -3,6 +3,7 @@ using Sitecore.Data.Items;
 using System.Collections.Generic;
 using Sitecore.Data.Fields;
 using Sitecore.Web.UI.WebControls;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Shared.BaseTemplate.Article;
 
 namespace UnderstoodDotOrg.Domain.SitecoreCIG.Poses.BaseTemplate.Article
 {
