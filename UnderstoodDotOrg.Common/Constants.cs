e2911e211bfeba9e164c480f45a3dfa0e7d361d7
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-08 20:28:09 +0000

    added constants for Membership
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@153 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Common/Constants.cs
+++ UnderstoodDotOrg.Common/Constants.cs
@@ -4,16 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UnderstoodDotOrg.Common {
-   public class Constants {
-       public static readonly string SCLANG_QUERY_STRING = "sc_lang";
+namespace UnderstoodDotOrg.Common
+{
+    public static class Constants
+    {
+        public static readonly string SCLANG_QUERY_STRING = "sc_lang";
 
-	   public static string CURRENT_INDEX_NAME
-	   {
-		   get
-		   {
-			   return string.Format("sitecore_{0}_index", Sitecore.Context.Database.Name.ToLower());
-		   }
-	   }
+        public static string CURRENT_INDEX_NAME
+        {
+            get
+            {
+                return string.Format("sitecore_{0}_index", Sitecore.Context.Database.Name.ToLower());
+            }
+        }
+
+        public static string MembershipProviderName
+        {
+            get
+            {
+                return "UnderstoodMembership";
+            }
+        }
     }
 }
