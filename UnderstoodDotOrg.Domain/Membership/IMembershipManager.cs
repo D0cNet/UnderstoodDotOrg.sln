60bf334a7ffa07f0f3c1e2e1c94fcd52843121d2
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-08 15:28:14 +0000

    stubs for membership manager
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@151 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Membership/IMembershipManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.Membership
+{
+    interface IMembershipManager
+    {
+        //authenticate
+
+        //add member
+        //add child to existing member
+        //?add interest
+        //?add issue
+        //?add diagnosis
+        //future: add member to child
+
+        //update member
+        //update child    
+    }
+}
