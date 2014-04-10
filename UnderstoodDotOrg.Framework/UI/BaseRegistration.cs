203ae8d43fbc109fc75944387ee96b89a05220b9
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-10 20:27:33 +0000

    Added base class for Registration pages to handle sessioning the currently registering user
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@161 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Framework/UI/BaseRegistration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Domain.Membership;
+
+namespace UnderstoodDotOrg.Framework.UI
+{
+    class BaseRegistration : BaseSublayout
+    {
+        private string sessionKey = "understood_org_registering_user";
+
+        public Member registeringUser 
+        {
+            get { return (Member)(Session[sessionKey]); }
+            set { Session[sessionKey] = value; }
+        }
+    }
+}
