13ac58d8d1905c05031153651eeb6a674421c676
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-17 21:03:07 +0000

    modifications for membership registration support
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@186 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Framework/UI/BaseRegistration.cs
+++ UnderstoodDotOrg.Framework/UI/BaseRegistration.cs
@@ -4,12 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnderstoodDotOrg.Domain.Membership;
+using UnderstoodDotOrg.Common;
 
 namespace UnderstoodDotOrg.Framework.UI
 {
     public class BaseRegistration : BaseSublayout
     {
         private string sessionKey = "understood_org_registering_user";
+
+        public static string YesButton { get { return DictionaryConstants.YesButtonText; } }
+        public static string NoButton { get { return DictionaryConstants.NoButtonText; } }
+        public static string GirlButton { get { return DictionaryConstants.GirlButtonText; } }
+        public static string BoyButton { get { return DictionaryConstants.BoyButtonText; } }
+        public static string NextButtonText { get { return DictionaryConstants.NextButtonText; } }
+        public static string InProgressText { get { return DictionaryConstants.InProgressButtonText; } }
 
         public Member registeringUser 
         {
