9267f1648c18c0774b5bff436656c21488490e72
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-23 18:47:43 +0000

    sublayout for My Profile page
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@211 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/MyProfile/My Profile.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="My_Profile.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.MyProfile.My_Profile" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
+
+<h2>hello worlds</h2>
---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/MyProfile/My Profile.ascx.cs
@@ -0,0 +1,12 @@
+﻿namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.MyProfile
+{
+    using System;
+
+    public partial class My_Profile : System.Web.UI.UserControl
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+    }
+}
