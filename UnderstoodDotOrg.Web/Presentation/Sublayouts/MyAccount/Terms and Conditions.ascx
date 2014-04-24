1a3d84edbce40e8e3e32d5b80fcadd18c40d706c
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-24 12:58:40 +0000

    sublayout for the T&Cs
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@219 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/MyAccount/Terms and Conditions.ascx
@@ -0,0 +1,2 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Terms_and_Conditions.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.MyAccount.Terms_and_Conditions" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/MyAccount/Terms and Conditions.ascx.cs
@@ -0,0 +1,12 @@
+﻿namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.MyAccount
+{
+    using System;
+
+    public partial class Terms_and_Conditions : System.Web.UI.UserControl
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+    }
+}
