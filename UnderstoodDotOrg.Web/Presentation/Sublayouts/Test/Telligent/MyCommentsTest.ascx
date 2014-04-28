f9dce6735d06952d2354dc22c1a2fd058ef60233
kanglin <kanglin@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-28 15:18:49 +0000

    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@234 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Test/Telligent/MyCommentsTest.ascx
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Test/Telligent/MyCommentsTest.ascx
@@ -2,23 +2,31 @@
 <!-- BEGIN PARTIAL: account-landing-mycomments -->
 <div class="landing-mycomments landing-modules">
   <header class="clearfix">
-    <h3>My Comments<span class="landing-module-count">100</span></h3>
+    <h3>My Comments<span class="landing-module-count"><asp:Label ID="lblCount" runat="server"></asp:Label></span></h3>
   </header>
   
-    <ul class="landing-module-items">
-      <li>
-        <span class="comment-wrap">
-          <a href="REPLACE" class="comment-link">sapiente nostrum laborum</a>
-          <p class="comment-description">maxime dolor esse et est praesentium laboriosam vel et unde aliquam at vel omnis voluptatem</p>
-        </span>
-      </li>
-      <li>
-        <span class="comment-wrap">
-          <a href="REPLACE" class="comment-link">veniam ut a</a>
-          <p class="comment-description">modi quia labore consectetur ipsa id magnam nam velit aspernatur consectetur dignissimos corrupti voluptate placeat</p>
-        </span>
-      </li>
-    </ul>
+    
+        <asp:Repeater ID="topComments" runat="server">
+            <HeaderTemplate>
+              <ul class="landing-module-items">
+            </HeaderTemplate>
+            <ItemTemplate>
+                   <li>
+                    <span class="comment-wrap">
+                        <asp:HyperLink NavigateUrl="#" CssClass="comment-link" ID="cLink" runat="server" />
+
+                        <p class="comment-description"><asp:Literal ID="cDesc" runat="server"></asp:Literal></p>
+                    </span>
+                  </li>
+
+            </ItemTemplate>
+            <FooterTemplate>
+             </ul>
+            </FooterTemplate>
+        </asp:Repeater>
+    
+  
+
     <div class="bottom"><a href="REPLACE">See All Comments</a></div>
   
 </div><!-- /.landing-notifications /.landing-modules -->
--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Test/Telligent/MyCommentsTest.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Test/Telligent/MyCommentsTest.ascx.cs
@@ -4,14 +4,95 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using Telligent.Evolution.Components;
+using Telligent.Evolution.Api.Services;
+using Telligent.Evolution.Extensibility.Api;
+using Telligent.Evolution.Extensibility.Api.Version1;
+using Telligent.Evolution.Extensibility.Api.Entities.Version1;
+using Telligent.Common;
+using System.Net;
+using System.Text;
+using System.Collections.Specialized;
+using System.Xml;
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Test.Telligent
 {
     public partial class MyCommentsTest : System.Web.UI.UserControl
     {
-        protected void Page_Load(object sender, EventArgs e)
+        private class CommentSnippet
         {
+            public string Title { get; set; }
+            public string Desc { get; set; }
+            public string Url { get; set; }
 
         }
+
+        protected override void OnInit(EventArgs e)
+        {
+            topComments.ItemDataBound += topComments_ItemDataBound;
+            base.OnInit(e);
+        }
+
+        void topComments_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+
+            if (e.Item.ItemType == ListItemType.Item)
+            {
+
+                HyperLink lnk = (HyperLink)e.Item.FindControl("cLink");
+                if (lnk != null)
+                {
+                    lnk.NavigateUrl = ((CommentSnippet)e.Item.DataItem).Url;
+                    lnk.Text = ((CommentSnippet)e.Item.DataItem).Title;
+
+                }
+
+                Literal lit = (Literal)e.Item.FindControl("cDesc");
+                if (lit != null)
+                    lit.Text = ((CommentSnippet)e.Item.DataItem).Desc;
+            }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+         
+            var webClient = new WebClient();
+            var apiKey = "d956up05xiu5l8fn7wpgmwj4ohgslp";
+           
+            var adminKey = String.Format("{0}:{1}", apiKey, "admin");
+            var adminKeyBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
+
+            //TODO: retrieve current logged in user
+            var userId ="admin";
+            //Todo: ID value should come from some inprocess maintained session of currently logged on user
+            var id = "2100";
+            webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
+            webClient.Headers.Add("Rest-Impersonate-User", userId);
+            var requestUrl = "http://telligentevolution/api.ashx/v2/comments.xml?UserId="+id;
+              
+            
+           
+            var xml = webClient.DownloadString(requestUrl);
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xml);
+            var nodes = xmlDoc.SelectNodes("Response/Comments[position()<=3] ");
+         // PagedList<Comment> commentList = PublicApi.Comments.Get(new CommentGetOptions() { UserId = 2100 });
+            lblCount.Text = nodes.Count.ToString();
+            List<CommentSnippet> commentSource = new List<CommentSnippet>();
+            foreach (XmlNode item in nodes)
+            {
+                
+                CommentSnippet cm = new CommentSnippet();
+                cm.Desc = item.SelectSingleNode("//Content/HtmlDescription").InnerText;
+                cm.Title = item.SelectSingleNode("//Content/HtmlName").InnerText;
+                cm.Url = item.SelectSingleNode("//Content/Url").InnerText;
+                commentSource.Add(cm);
+            }
+
+            topComments.DataSource = commentSource;
+            topComments.DataBind();
+
+        }
+
+
     }
 }
