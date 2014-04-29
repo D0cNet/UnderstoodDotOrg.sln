2e1085e85c2f5821aed9df684b23ce6c20276e13
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-29 14:05:32 +0000

    added comments
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@245 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Domain/Membership/MembershipConstructors.cs
+++ UnderstoodDotOrg.Domain/Membership/MembershipConstructors.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 
 namespace UnderstoodDotOrg.Domain.Membership
 {
+    /// <summary>
+    /// Provides custom constructor for Child, which ensures ChildId is populated when new instance is created
+    /// </summary>
     public partial class Child
     {
+        /// <summary>
+        /// Creates a new instance of Child, and ensures ChildId is populated
+        /// </summary>
         public Child()
         {
             this.ChildId = Guid.NewGuid();
@@ -15,8 +21,14 @@ namespace UnderstoodDotOrg.Domain.Membership
         }
     }
 
+    /// <summary>
+    /// Provides custom constructor for Member, which ensures MemberId is populated when new instance is created
+    /// </summary>
     public partial class Member
     {
+        /// <summary>
+        /// Creates a new instance of Member, and ensures MemberId is populated
+        /// </summary>
         public Member()
         {
             this.MemberId = Guid.NewGuid();
