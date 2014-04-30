352bf695e3f9611325f41a2b91a9f043adf10a89
jrathore <jrathore@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-30 14:22:23 +0000

    header section page topic
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@266 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Section/SectionPageTopic.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Section/SectionPageTopic.ascx.cs
@@ -1,14 +1,27 @@
-﻿using System;
+﻿using Sitecore.Data.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.LandingPages;
+using UnderstoodDotOrg.Common.Extensions;
+using UnderstoodDotOrg.Domain.SitecoreCIG;
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Section {
     public partial class SectionPageTitle : System.Web.UI.UserControl {
         protected void Page_Load(object sender, EventArgs e) {
-
+            SectionLandingPageItem contextItem = Sitecore.Context.Item;
+            Item homePage = MainsectionItem.GetHomeItem();
+            if (homePage != null) {
+                hlBackLink.NavigateUrl = homePage.GetUrl();
+            }
+            if(contextItem != null){
+                scTopicTitle.Text = contextItem.DisplayName;
+            }
         }
+
+        
     }
 }
