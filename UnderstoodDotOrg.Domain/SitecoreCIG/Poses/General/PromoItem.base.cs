23f45975447c386e3a024d13b6d57e8997234f25
ybharote <ybharote@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-04-30 14:22:47 +0000

    added CIG file for PromoItem.base.cs
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@267 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/SitecoreCIG/Poses/General/PromoItem.base.cs
@@ -0,0 +1,88 @@
+using System;
+using Sitecore.Data.Items;
+using System.Collections.Generic;
+using Sitecore.Data.Fields;
+using Sitecore.Web.UI.WebControls;
+using CustomItemGenerator.Fields.LinkTypes;
+using CustomItemGenerator.Fields.ListTypes;
+using CustomItemGenerator.Fields.SimpleTypes;
+//using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Base.BasePageItems;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.BasePageItems;
+
+namespace UnderstoodDotOrg.Domain.SitecoreCIG.Poses.General
+{
+public partial class PromoItem : CustomItem
+{
+
+public static readonly string TemplateId = "{667D0991-EAE8-43EA-A1A9-52716195EFC1}";
+
+#region Inherited Base Templates
+
+private readonly ContentPageItem _ContentPageItem;
+public ContentPageItem ContentPage { get { return _ContentPageItem; } }
+
+#endregion
+
+#region Boilerplate CustomItem Code
+
+public PromoItem(Item innerItem) : base(innerItem)
+{
+	_ContentPageItem = new ContentPageItem(innerItem);
+
+}
+
+public static implicit operator PromoItem(Item innerItem)
+{
+	return innerItem != null ? new PromoItem(innerItem) : null;
+}
+
+public static implicit operator Item(PromoItem customItem)
+{
+	return customItem != null ? customItem.InnerItem : null;
+}
+
+#endregion //Boilerplate CustomItem Code
+
+
+#region Field Instance Methods
+
+
+public CustomTextField PromoTitle
+{
+	get
+	{
+		return new CustomTextField(InnerItem, InnerItem.Fields["Promo Title"]);
+	}
+}
+
+
+public CustomTextField PromoURL
+{
+	get
+	{
+		return new CustomTextField(InnerItem, InnerItem.Fields["Promo URL"]);
+	}
+}
+
+
+public CustomCheckboxField IncludeinPromoTool
+{
+	get
+	{
+		return new CustomCheckboxField(InnerItem, InnerItem.Fields["Include in PromoTool"]);
+	}
+}
+
+
+public CustomImageField PromoImage
+{
+    get
+    {
+        return new CustomImageField(InnerItem, InnerItem.Fields["Promo Image"]);
+    }
+}
+
+
+#endregion //Field Instance Methods
+}
+}
