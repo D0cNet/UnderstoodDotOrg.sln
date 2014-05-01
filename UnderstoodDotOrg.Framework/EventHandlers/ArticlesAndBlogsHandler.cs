ddbfc442115d7fc1c5f3383e262ec48ae9610525
dallegrone <dallegrone@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-01 15:45:00 +0000

    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@285 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Framework/EventHandlers/ArticlesAndBlogsHandler.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Framework.EventHandlers
+{
+    class ArticlesAndBlogsHandler
+    {
+        protected void OnItemSaved(object sender, EventArgs args)
+        {
+            var itm = Event.ExtractParameter(args, 0) as Item;
+            if (itm != null)
+            {
+                
+            }
+
+        }
+    }
+}
