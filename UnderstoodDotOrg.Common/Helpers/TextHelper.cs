2bf634bdd105cf0d45462fe0e3c4ed3b9f32fddb
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-13 19:01:34 +0000

    Adding missing file
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@473 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Common/Helpers/TextHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Common.Helpers
+{
+    public class TextHelper
+    {
+        public static string ToTitleCase(string input)
+        {
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+        }
+    }
+}
