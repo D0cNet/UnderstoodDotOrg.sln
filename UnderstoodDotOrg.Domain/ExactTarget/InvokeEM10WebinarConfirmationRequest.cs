186507554e1a49847457900c403f4416b8c9d3d1
jtesta <jtesta@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-14 16:27:26 +0000

    sorry i broke the build :(
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@493 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/ExactTarget/InvokeEM10WebinarConfirmationRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.ExactTarget
+{
+	public class InvokeEM10WebinarConfirmationRequest :BaseRequest
+	{
+		public string WebinarModule { get; set; }
+	}
+}
