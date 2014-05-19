7edeb8bb7877e2182ae31d4c091bcc78b3880f3c
jrathore <jrathore@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-19 08:53:23 +0000

    Expert live: Webnier video resource control
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@638 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/ExpertLive/VideoResources.ascx
@@ -0,0 +1,54 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="VideoResources.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.ExpertLive.VideoResources" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
+<div class="player-container">
+    <div class="player">
+        <asp:Literal runat="server" ID="ltBrightCovePlayer"></asp:Literal>
+    </div>
+</div>
+<div class="col col-18 offset-3">
+    <!-- BEGIN PARTIAL: transcript-control -->
+    <div class="transcript-container Video">
+        <div class="read-more mobile-close">
+            <a href="REMOVE">
+                <asp:Literal runat="server" ID="ltVideoDetailShow"></asp:Literal><i class="icon-arrow-up-blue"></i></a>
+        </div>
+        <div class="transcript-wrap clearfix rs_read_this">
+            <sc:fieldrenderer id="frVideoTranscript" runat="server" fieldname="Video Transcript" />
+
+        </div>
+        <div class="read-more read-more-bottom"></div>
+    </div>
+    <!-- END PARTIAL: transcript-control -->
+</div>
+<script type="text/javascript">
+    runMobileCompatibilityScript('birghtcovePlayerObject');
+</script>
+<script type="text/javascript">brightcove.createExperiences();</script>
+<script type="text/javascript">
+    var player;
+    var modVP;
+
+    function myTemplateLoaded(experienceID) {
+        player = brightcove.api.getExperience(experienceID);
+        modVP = player.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+    }
+
+    $('.video_resource_container .video_rotator .slideshow ul li:first').addClass('active-vd');
+
+    $('.video_resource_container .video_rotator .slideshow ul li input').click(function () {
+        $('.video_resource_container .video_rotator .slideshow ul li').removeClass('active-vd');
+        $(this).parent().addClass('active-vd');
+    });
+
+    function onTemplateReady(evt) {
+    }
+
+    function onMediaComplete(videoid, title, desc) {
+        $('.video_title').empty();
+        $('.video_title').html(title);
+        $('.video_para').empty();
+        $('.video_para').html(desc);
+        modVP.loadVideoByID(videoid);
+    }
+    </script>
+
---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/ExpertLive/VideoResources.ascx.cs
@@ -0,0 +1,65 @@
+﻿using Sitecore.Data.Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UnderstoodDotOrg.Common;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ExpertLive;
+
+namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.ExpertLive {
+    public partial class VideoResources : System.Web.UI.UserControl {
+        protected void Page_Load(object sender, EventArgs e) {
+          ltBrightCovePlayer.Text =  GetEmbedCode("480", "270");
+          ltVideoDetailShow.Text = DictionaryConstants.CloseTranscriptLabel;
+          WebinarEventPageItem contextItem = Sitecore.Context.Item;
+          if (contextItem != null) {
+              frVideoTranscript.Item = contextItem;
+          }
+        }
+
+        private const string BackgroundColor = "#ffffff";
+        private static string CreateEmbedID() {
+            Random r = new Random();
+            return "BrightcoveVideo_" + r.Next(1001).ToString();
+        }
+
+        /// <summary>
+        /// This will build an html object tag based on the information provided
+        /// </summary>
+        public static string GetEmbedCode(string playerWidth, string playerHeight) {
+
+            StringBuilder embed = new StringBuilder();
+
+            
+                //this one works
+                embed.AppendLine("<!-- Start of Brightcove Player -->");
+                embed.AppendLine("");
+                embed.AppendLine("<div style=\"display:none\"></div>");
+                embed.AppendLine("<script language=\"JavaScript\" type=\"text/javascript\" src=\"http://admin.brightcove.com/js/BrightcoveExperiences.js\"></script>");
+                embed.AppendLine("<object id=\"" + CreateEmbedID() + "\" class=\"BrightcoveExperience\">");
+                embed.AppendLine("<param name=\"bgcolor\" value=\"" + BackgroundColor + "\" />");
+                embed.AppendLine("<param name=\"width\" value=\"" + playerWidth + "\" />");
+                embed.AppendLine("<param name=\"height\" value=\"" + playerHeight + "\" />");
+                embed.AppendLine("<param name=\"playerID\" value=\"3203925133001\" />");
+                embed.AppendLine("<param name=\"playerKey\" value=\"AQ~~,AAAC6NDP1nE~,dOSiqHy89Sli4ZPOUFfVGW6O9wJ4rR6y\" />");
+                embed.AppendLine("<param name=\"@videoPlayer\" value=\"3203925031001\"/>");
+                embed.AppendLine("<param name=\"isVid\" value=\"true\" />");
+                embed.AppendLine("<param name=\"autoStart\" value=\"false\" />");
+                embed.AppendLine("<param name=\"isUI\" value=\"true\" />");
+                embed.AppendLine("<param name=\"dynamicStreaming\" value=\"true\" />");
+                embed.AppendLine("<param name=\"templateLoadHandler\" value=\"myTemplateLoaded\" />");
+                embed.AppendLine("<param name=\"allowScriptAccess\" value=\"always\" />");
+                embed.AppendLine("<param name=\"includeAPI\" value=\"true\" />");
+                embed.AppendLine("<param name=\"wmode\" value=\"opaque\" />");
+                embed.AppendLine("</object>");
+
+                embed.AppendLine("");
+                embed.AppendLine("<!-- End of Brightcove Player -->");
+            
+            return embed.ToString();
+        }
+    }
+}
