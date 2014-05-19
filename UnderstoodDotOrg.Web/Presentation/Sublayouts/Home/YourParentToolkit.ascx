b84020f34b52dd782a56ca6afd6e68af0b78ba37
jrathore <jrathore@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-19 16:25:53 +0000

    Home page: Your parent tool kit slider
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@665 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/YourParentToolkit.ascx
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/YourParentToolkit.ascx
@@ -1,12 +1,15 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="YourParentToolkit.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.Home.YourParentToolkit" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
 <!-- BEGIN MODULE: Your Parent Toolkit -->
 <div class="container parent-toolkit">
     <div class="row">
         <div class="col col-24">
 
             <header class="header-parent-toolkit">
-                <h2>Your Parent Toolkit</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit. magna aliqua Lorem ipsum dolor sit amet, consectetur adipisicing elit. magna aliqua.</p>
+                <h2>
+                    <sc:FieldRenderer ID="frParentToolKitHeading" runat="server" FieldName="Your Parent Toolkit Heading" />
+                </h2>
+                <sc:FieldRenderer ID="frParentToolDetail" runat="server" FieldName="Your Parent Toolkit Details" />
             </header>
             <!-- .header-parent-toolkit -->
 
@@ -65,26 +68,35 @@
             <!-- BEGIN PARTIAL: toolkit -->
             <div class="tools-container">
                 <div id="toolkit-slides-container">
-                    <div class="slide">
-                        <ul>
+                    <asp:Repeater ID="rptEventCarousel" runat="server" OnItemDataBound="rptEventCarousel_ItemDataBound">
+                        <ItemTemplate>
+                            <asp:Literal runat="server" ID="litDevStart" Visible="false" Text="<div class='slide'>"></asp:Literal>
+                            <asp:Literal runat="server" ID="litStartUL" Visible="false" Text="<ul>"></asp:Literal>
                             <li>
-                                <div class="icon support-plan"><a href="REPLACE.html">Your Support Plan</a></div>
+                                <div class="icon" runat="server" id="divIcon">
+                                    <sc:Link ID="scLink" runat="server" Field="Link" />                                      
+                                </div>
                             </li>
-                            <li>
-                                <div class="icon observation-logs"><a href="REPLACE.html">Observation Log</a></div>
-                            </li>
-                        </ul>
-                        <ul>
-                            <li>
-                                <div class="icon childs-world"><a href="REPLACE.html">A Child's World</a></div>
-                            </li>
-                            <li>
-                                <div class="icon rate-schools"><a href="REPLACE.html">Rate Schools</a></div>
-                            </li>
-                        </ul>
-                    </div>
+                            <asp:Literal runat="server" ID="litEndUL" Visible="false" Text="</ul>"></asp:Literal>
+                            <asp:Literal runat="server" ID="litDivEnd" Visible="false" Text="</div>"></asp:Literal>
+                        </ItemTemplate>
+                    </asp:Repeater>
+
+
+
+
+
+
+
+
+
+
+
+
+
+                    <%--</div>--%>
                     <!-- .slide -->
-                    <div class="slide">
+                    <%--  <div class="slide">
                         <ul>
                             <li>
                                 <div class="icon support-plan"><a href="REPLACE.html">Your Support Plan</a></div>
@@ -101,7 +113,7 @@
                                 <div class="icon rate-schools"><a href="REPLACE.html">Rate Schools</a></div>
                             </li>
                         </ul>
-                    </div>
+                    </div>--%>
                     <!-- .slide -->
                 </div>
                 <!-- #toolkit-slides-container -->
--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/YourParentToolkit.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/YourParentToolkit.ascx.cs
@@ -4,10 +4,123 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.LandingPages;
+using UnderstoodDotOrg.Common.Extensions;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ExpertLive;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ExpertLive.Base;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.General;
+using Sitecore.Web.UI.WebControls;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Folders;
+using Sitecore.Data.Items;
+using System.Web.UI.HtmlControls;
 
-namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Home {
-    public partial class YourParentToolkit : System.Web.UI.UserControl {
-        protected void Page_Load(object sender, EventArgs e) {
+namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Home
+{
+    public partial class YourParentToolkit : System.Web.UI.UserControl
+    {
+        int toolsCount = 0;
+        int nextUlStart = 0;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HomePageItem ContextItem = Sitecore.Context.Item;
+            List<Item> toolKititems = null;
+            if (ContextItem.YourParentToolkitList != null)
+            {
+                toolKititems = ContextItem.YourParentToolkitList.Item.GetChildren().ToList();
+                if (toolKititems.Any())
+                {
+                    toolsCount = toolKititems.Count();
+                    rptEventCarousel.DataSource = toolKititems;
+                    rptEventCarousel.DataBind();
+
+                }
+            }
+
+        }
+
+        protected void rptEventCarousel_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.IsItem())
+            {
+                ParentToolkitFolderItem objParent = e.Item.DataItem as Item;
+                Item objNav = e.Item.DataItem as Item;
+                NavigationLinkItem objNavItem = new NavigationLinkItem(objNav);
+                Link scLink = e.FindControlAs<Link>("scLink");
+                Literal litDevStart = e.FindControlAs<Literal>("litDevStart");
+                Literal litStartUL = e.FindControlAs<Literal>("litStartUL");
+                Literal litEndUL = e.FindControlAs<Literal>("litEndUL");
+                Literal litDivEnd = e.FindControlAs<Literal>("litDivEnd");
+                HyperLink hlLink = e.FindControlAs<HyperLink>("hlLink");
+                HtmlGenericControl divIcon = e.FindControlAs<HtmlGenericControl>("divIcon");
+
+                if (litDevStart != null && litStartUL != null && litEndUL != null && litDivEnd != null)
+                {
+                    int cindex = e.Item.ItemIndex + 1;
+                    if (cindex == 1)
+                    {
+                        litDevStart.Visible = litStartUL.Visible = true;
+                    }
+                    if ((cindex) % 2 == 0 && (cindex) != 1)
+                    {
+                        litEndUL.Visible = true;
+                    }
+                    //if ((cindex) % 3 == 0 && (cindex) != 1) {
+                    //    litStartUL.Visible = true;
+                    //}
+                    //if ((cindex) == 3) {
+                    //    litStartUL.Visible = true;
+                    //    nextUlStart = cindex + 4;
+                    //}
+                    //if (cindex == nextUlStart) {
+                    //    litStartUL.Visible = true;
+                    //}
+
+                    if ((cindex) % 2 == 1 && (cindex) != 1)
+                    {
+                        litStartUL.Visible = true;
+                    }
+
+
+
+                    if ((cindex) % 4 == 0 && (cindex) != 1)
+                    {
+                        litDivEnd.Visible = true;
+                    }
+
+                    //if ((cindex) % 5 == 0 && (cindex) != 1) {
+                    //    litDevStart.Visible = true;
+                    //    litStartUL.Visible = true;
+                    //}
+                    if ((cindex) % 4 == 1 && (cindex) != 1)
+                    {
+                        litDevStart.Visible = true;
+                        //litStartUL.Visible = true;
+                    }
+
+                    //if (rptEventCarousel.Items.Count == cindex) {
+                    if (toolsCount == cindex)
+                    {
+                        litEndUL.Visible = true;
+                        litDivEnd.Visible = true;
+                    }
+
+                }
+                if (objNav != null)
+                {
+                    if (divIcon != null)
+                    {
+                        //divIcon.Attributes.Add("style", "background: url(" + objNavItem.Image.MediaUrl + ") no-repeat scroll 0 0 / 100px 100px rgba(0, 0, 0, 0)");
+                        divIcon.Attributes.Add("style", "background-image: url(" + objNavItem.Image.MediaUrl + ");background-repeat:no-repeat;");
+                    }
+                    if (scLink != null)
+                    {
+                        scLink.Item = objNav;
+                    }
+                }
+
+
+            }
 
         }
     }
