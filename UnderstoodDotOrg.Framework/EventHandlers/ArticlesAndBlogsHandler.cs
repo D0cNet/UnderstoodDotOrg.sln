7ea7c8d4995d6f9bef001fc53b0774ae807e51b3
zkniebel <zkniebel@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-22 15:56:29 +0000

    Updated ArticlesAndBlogsHandler.cs in attempt to fix Republish issues. Note that although this did not fix the republish issues, the logic cleanup and updates are still being committed as other potential issues were prevented.
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@783 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Framework/EventHandlers/ArticlesAndBlogsHandler.cs
+++ UnderstoodDotOrg.Framework/EventHandlers/ArticlesAndBlogsHandler.cs
@@ -21,47 +21,37 @@ namespace UnderstoodDotOrg.Framework.EventHandlers
     {
         protected void OnItemSaved(object sender, EventArgs args)
         {
-            // item.path.paths
             var itm = Sitecore.Events.Event.ExtractParameter(args, 0) as Item;
-            int blogId = 0;
 
-            if (itm.InheritsFromType(DefaultArticlePageItem.TemplateId.ToString())
+            if (itm.InheritsFromType(DefaultArticlePageItem.TemplateId)
                 || itm.InheritsTemplate(BehaviorAdvicePageItem.TemplateId))
             {
-                if (itm["BlogId"] == "" || itm["BlogId"] == "1")
+                if (itm["BlogId"] == string.Empty)
                 {
-                    blogId = 4;
-                    CreateTelligentPost(itm, blogId);
+                    CreateTelligentPost(itm, 4); //blog id should be 4
                 }
             }
-            else if (itm.InheritsFromType(BlogsPostPageItem.TemplateId.ToString()))
+            else if (itm.InheritsFromType(BlogsPostPageItem.TemplateId))
             {
-                if (itm["BlogId"] == "")
+                if (itm["BlogId"] == string.Empty)
                 {
                     switch (itm.Parent.ID.ToString())
                     {
                         case "{37478172-CCDF-454E-BABA-D56096EBE8F9}":
-                            blogId = 1;
+                            CreateTelligentPost(itm, 1); //blog id should be 1
                             break;
                         case "{23DC4EBA-B296-46A7-AC68-D813C9931AF0}":
-                            blogId = 2;
+                            CreateTelligentPost(itm, 2); //blog id should be 2
                             break;
                         case "{A720AAA9-8AC8-4851-A873-0E0F158C61BD}":
-                            blogId = 3;
+                            CreateTelligentPost(itm, 3); //blog id should be 3
                             break;
                         default:
                             return;
                     }
-                    CreateTelligentPost(itm, blogId);
                 }
-                //if (blogId != 0 && (itm.TemplateID.ToString() == BlogsPostPageItem.TemplateId || itm.TemplateID.ToString() == DefaultArticlePageItem.TemplateId))
-                //{
-                    //CreateTelligentPost(itm, blogId);
-                //}
             }
         }
-        
-        
 
         private void CreateTelligentPost(Item item, int blogId)
         {
@@ -69,39 +59,38 @@ namespace UnderstoodDotOrg.Framework.EventHandlers
             {
                 var webClient = new WebClient();
 
-                // replace the "admin" and "Admin's API key" with your valid user and apikey!
-              //  var adminKey = string.Format("{0}:{1}", Settings.GetSetting(Constants.Settings.TelligentAdminApiKey), "admin");
-                var adminKeyBase64 = CommunityHelper.TelligentAuth(); //Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
+                var adminKeyBase64 = CommunityHelper.TelligentAuth();
 
                 webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
-                var requestUrl = string.Format("{0}api.ashx/v2/blogs/{1}/posts.xml", Settings.GetSetting(Constants.Settings.TelligentConfig), blogId);
+                var requestUrl = string.Format(
+                    "{0}api.ashx/v2/blogs/{1}/posts.xml", 
+                    Settings.GetSetting(Constants.Settings.TelligentConfig), 
+                    blogId);
 
                 var values = new NameValueCollection();
                 values["Title"] = item.Name;
                 values["Body"] = item.Paths.Path;
-
 
                 var xml = Encoding.UTF8.GetString(webClient.UploadValues(requestUrl, values));
 
                 var xmlDoc = new XmlDocument();
                 xmlDoc.LoadXml(xml);
 
-                XmlNodeList nodes = xmlDoc.SelectNodes("Response/BlogPost");
-                foreach (XmlNode xn in nodes)
-                {
-                    try
-                    {
-                        item.Editing.BeginEdit();
-                        item["BlogPostId"] = xn["Id"].InnerText;
-                        item["BlogId"] = blogId.ToString();
-                        item["ContentId"] = xn["ContentId"].InnerText;
+                var node = xmlDoc.SelectSingleNode("Response/BlogPost");
+                var blogPostId = node["Id"].InnerText;
+                var contentId = node["ContentId"].InnerText;
 
-                    }
-                    finally
-                    {
-                        item.Editing.EndEdit();
-                    }
+                item.Editing.BeginEdit();
+                try
+                {
+                    item["BlogPostId"] = blogPostId;
+                    item["BlogId"] = blogId.ToString();
+                    item["ContentId"] = contentId;
                 }
+                catch
+                {        
+                }
+                item.Editing.EndEdit();
             }
             catch
             {
