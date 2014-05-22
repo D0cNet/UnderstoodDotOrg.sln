81d43061ef0b99c7c479bf80aa95d3ff3078eeb9
mursino <mursino@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-22 14:32:07 +0000

    added class for event search result items
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@778 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Search/EventPage.cs
@@ -0,0 +1,18 @@
+﻿using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Common;
+
+namespace UnderstoodDotOrg.Domain.Search
+{
+    public class EventPage : SearchResultItem
+    {
+        [IndexField("event_date")]
+        public DateTime EventDate { get; set; }
+    }
+}
