16d0f18c109659643f7ff721bfb5979ae07ad166
ajain <ajain@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-23 12:51:36 +0000

    testing
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@827 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/HomeHeroCarousel.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Home/HomeHeroCarousel.ascx.cs
@@ -31,6 +31,7 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Home {
                     GetSliderItem(ContextItem);
 
                     var childIssues = GetFilters(ChildIssueItem.TemplateId).Select(i => (ChildIssueItem)i);
+                    Response.Write("ChildIssue " + childIssues.Count().ToString() + "<br/>");
                     if (childIssues != null && childIssues.Any()) {
                         rptChildIssues.Visible = true;
                         rptChildIssues.DataSource = childIssues;
@@ -41,6 +42,8 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Home {
                     }
 
                     var grades = GetFilters(GradeLevelItem.TemplateId).Select(i => (GradeLevelItem)i);
+                    Response.Write("Grades " + grades.Count().ToString() + "<br/>");
+
                     if (grades != null && grades.Any()) {
                         rptGrades.Visible = true;
                         rptGrades.DataSource = grades;
@@ -51,6 +54,8 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Home {
                     }
 
                     BindGradesDDL();
+
+                    Response.End();
                 }
             }
             //if (CurrentMember != null && CurrentMember.Children != null && CurrentMember.Children.Any()) {
