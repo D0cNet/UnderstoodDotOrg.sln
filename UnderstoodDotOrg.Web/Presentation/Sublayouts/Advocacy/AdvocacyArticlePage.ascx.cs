11cd1ef225342bf64e032949d5b1bfd77f7d434a
zkniebel <zkniebel@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-27 17:27:52 +0000

    Removed old Promotionals List sublayout, Readded AdvocacyArticlePage
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@897 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Advocacy/AdvocacyArticlePage.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Advocacy/AdvocacyArticlePage.ascx.cs
@@ -4,11 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using UnderstoodDotOrg.Framework.UI;
 
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Advocacy
 {
-    public partial class AdvocacyArticlePage : BaseSublayout
+    public partial class AdvocacyArticlePage : System.Web.UI.UserControl
     {
         protected void Page_Load(object sender, EventArgs e)
         {
