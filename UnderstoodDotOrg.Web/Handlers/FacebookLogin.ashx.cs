8753cd5e642eb2ac0415dcd9bc5202efb5836982
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-27 17:49:31 +0000

    added handler for consuming response from Facebook
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@901 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Handlers/FacebookLogin.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UnderstoodDotOrg.Web.Handlers
+{
+    /// <summary>
+    /// Summary description for FacebookLogin
+    /// </summary>
+    public class FacebookLogin : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            var accessToken = context.Request["accessToken"];
+            context.Session["AccessToken"] = accessToken;
+
+            context.Response.Redirect(context.Request.UrlReferrer.ToString());
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
