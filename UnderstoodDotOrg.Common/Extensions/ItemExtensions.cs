0d0c5be0f19e18fefadd11352cb896828eb3c48b
ajain <ajain@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-05-29 10:53:01 +0000

    Article list by template version v1.
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@948 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Common/Extensions/ItemExtensions.cs
+++ UnderstoodDotOrg.Common/Extensions/ItemExtensions.cs
@@ -208,5 +208,14 @@ namespace UnderstoodDotOrg.Common.Extensions
                 return null;
             }
         }
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+                  (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector) {
+            HashSet<TKey> seenKeys = new HashSet<TKey>();
+            foreach (TSource element in source) {
+                if (seenKeys.Add(keySelector(element))) {
+                    yield return element;
+                }
+            }
+        }
     }
 }
