ec46d16e7a83b5db22a210974c9810756aa15c5f
bgarnier <bgarnier@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-01 16:23:25 +0000

    Adding in models for saving quiz data
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1023 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Understood/Quiz/Quiz.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.Understood.Quiz
+{
+    public class Quiz
+    {
+        public Guid MemberId { get; set; }
+        public Guid QuizID { get; set; }
+
+        public virtual ICollection<QuizItem > MemberAnswers { get; set; }
+    }
+
+}
---
+++ UnderstoodDotOrg.Domain/Understood/Quiz/QuizItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Domain.Understood.Quiz
+{
+    public class QuizItem
+    {
+        public Guid QuestionId { get; set; }
+        public string AnswerValue { get; set; }
+        public DateTime AnsweredOn { get; set; }
+        public QuizItem()
+        { 
+        }
+        public QuizItem(Guid Question, string Answer)
+        {
+            this.QuestionId = Question;
+            this.AnswerValue = Answer;
+            this.AnsweredOn = DateTime.Now;
+        }
+
+    }
+    
+}
