be3730d0949936f3e23255183f9822e5b9004ade
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-01 15:22:52 +0000

    added MembershipHelper to store some common non-navigation related stuff
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1020 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Common/Helpers/MembershipHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstoodDotOrg.Common.Helpers
+{
+    public static class MembershipHelper
+    {
+        public static string AddOrdinalIndicator(string Value)
+        {
+            switch (Value)
+            {
+                case "1":
+                    Value += "st";
+                    break;
+                case "2":
+                    Value += "nd";
+                    break;
+                case "3":
+                    Value += "rd";
+                    break;
+                default:
+                    Value += "th";
+                    break;
+            }
+
+            return Value;
+        }
+
+        public static string AddOrdinalIndicator(int Value)
+        {
+            return AddOrdinalIndicator(Value.ToString());
+        }
+    }
+}
