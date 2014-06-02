12b0baa4c70ec360226980c4187b539406d9a02d
bgarnier <bgarnier@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-02 20:48:03 +0000

    One script to alter the member table, one script to update the data
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1058 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Build/Packages/06022014/1_Run_First_ALTER_MEMBER_TABLE.sql
@@ -0,0 +1,45 @@
+BEGIN TRANSACTION
+SET QUOTED_IDENTIFIER ON
+SET ARITHABORT ON
+SET NUMERIC_ROUNDABORT OFF
+SET CONCAT_NULL_YIELDS_NULL ON
+SET ANSI_NULLS ON
+SET ANSI_PADDING ON
+SET ANSI_WARNINGS ON
+COMMIT
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.Members ADD
+	PreferedLanguage uniqueidentifier  NULL
+GO
+ALTER TABLE dbo.Members ADD CONSTRAINT
+	PreferedLanguage DEFAULT (N'AF584191-45C9-4201-8740-5409F4CF8BDD') FOR PreferedLanguage
+go
+ALTER TABLE dbo.Members ADD
+	AgreedToSignUpTerms bit NOT NULL
+GO
+ALTER TABLE dbo.Members ADD CONSTRAINT
+	AgreedToSignUpTerms DEFAULT 0 FOR AgreedToSignUpTerms
+GO
+ALTER TABLE dbo.Members ADD
+	MobilePhoneNumber nchar(15)  NULL
+GO
+ALTER TABLE dbo.Members ADD
+	Subscribed_DailyDigest bit  NULL
+GO
+ALTER TABLE dbo.Members ADD CONSTRAINT
+	Subscribed_DailyDigest DEFAULT 0 FOR Subscribed_DailyDigest
+GO
+ALTER TABLE dbo.Members ADD
+	Subscribed_WeeklyDigest bit  NULL
+GO
+ALTER TABLE dbo.Members ADD CONSTRAINT
+	Subscribed_WeeklyDigest DEFAULT 0 FOR Subscribed_WeeklyDigest
+GO
+
+ALTER TABLE dbo.Members SET (LOCK_ESCALATION = TABLE)
+GO
+
+
+
+COMMIT
---
+++ UnderstoodDotOrg.Build/Packages/06022014/2_Run_Second_UpdateMemberTableData.sql
@@ -0,0 +1,5 @@
+UPDATE Members
+SET       PreferedLanguage = 'AF584191-45C9-4201-8740-5409F4CF8BDD', 
+AgreedToSignUpTerms = 0, 
+Subscribed_DailyDigest = 0, 
+Subscribed_WeeklyDigest = 0
