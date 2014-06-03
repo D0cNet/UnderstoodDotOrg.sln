2881a198758b64a1ca953e1d4bfbce75290a970f
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-03 20:30:24 +0000

    Added ajax paging to Experts landing page
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1083 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Includes/js/oasis.js
+++ UnderstoodDotOrg.Web/Presentation/Includes/js/oasis.js
@@ -179,6 +179,57 @@ jQuery(function () {
 	$(document).ready(init);
 })(jQuery);
 
+// Experts listing
+(function ($) {
+    var currentPage = 1;
+	var inProgress = false;
+	var $trigger = $("#experts-show-more-results");
+	var $container, $showMoreContainer, path, topic, grade, issue;
+	
+	function init() {
+		if ($trigger.length > 0) {
+			path = $trigger.data('path');
+			$container = $("#" + $trigger.data('container'));
+			$showMoreContainer = $trigger.closest(".show-more");
+			
+			$trigger.on("click", showMore_clickHandler);
+		}
+	}
+	
+	function showMore_clickHandler(e) {
+		e.preventDefault();
+		
+		if (inProgress) {
+			return;
+		}
+		
+		inProgress = true;
+
+		$('html,body').animate({scrollTop: $showMoreContainer.offset().top - 40}, 500);
+		
+		var data = {
+			'page' : currentPage + 1
+		};
+		
+		$.ajax({
+			url: path,
+			data: data,
+			method: 'GET'
+		}).done(function (data) {
+			currentPage++;
+			if ($(data).filter('input[type="hidden"]').length == 0) 
+			{
+				$showMoreContainer.hide();
+			}
+			$container.append(data);
+		}).always(function() {
+			inProgress = false;
+		});
+	}
+	
+	$(document).ready(init);
+})(jQuery);
+
 //"Show More" functionality for repeaters
 var showCount = 3;
 $(document).ready(function () {
