c4398969d04d2a2d66aeaa1ac5f9937a8c773010
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-04 12:01:12 +0000

    Added new expert detail sublayout
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1096 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/About/ExpertsDetailPage.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.AboutPages;
+using UnderstoodDotOrg.Framework.UI;
+
+namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.About
+{
+    public partial class ExpertsDetailPage : BaseSublayout<ExpertDetailPageItem>
+    {
+        private void Page_Load(object sender, EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+    }
+}
