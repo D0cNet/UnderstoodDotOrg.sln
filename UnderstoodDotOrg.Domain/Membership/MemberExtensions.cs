0597dc85fd1a241531b5b52c03d0de58c81b3304
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-14 19:52:35 +0000

    Moved extensions to member model to MemberModelExtensions, created new static class called MemberExtensions to contain GetMemberPublicProfile() method
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1340 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Domain/Membership/MemberExtensions.cs
+++ UnderstoodDotOrg.Domain/Membership/MemberExtensions.cs
@@ -1,74 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UnderstoodDotOrg.Domain.Understood.Quiz;
-using System.Data.Spatial; 
+﻿using UnderstoodDotOrg.Common;
+using UnderstoodDotOrg.Common.Extensions;
+using UnderstoodDotOrg.Domain.SitecoreCIG;
 
 namespace UnderstoodDotOrg.Domain.Membership
 {
-    using System;
-    using System.Collections.Generic;
-
-    public partial class Member
+    public static class MemberExtensions
     {
-       //private bool _dataIsDirty = false;
-        
-
-
-        /// <summary>
-        /// bg: added in to track our Unauthorized Member flag.
-        // .Net Membership already has a Comment column in the db.
-        /// </summary>
-        public string Comment { get ; set; }
-
-        /// <summary>
-        /// bg: I wanted to keep the email address inside the member instead of passing it around as a parameter
-        /// </summary>
-        public string Email { get; set; }
-
-        /// <summary>
-        /// bg: 
-        /// </summary>
-        public string Password { get; set; }
-
-        /// <summary>
-        //    bg: sitecore GUID for english/spanish/etc.
-        //    currently only being set when a user subscribes for a personalized newsletter
-        /// </summary>
-        public Guid PreferedLanguage { get; set; }
-        public bool AgreedToSignUpTerms { get; set; }
-        public string MobilePhoneNumber { get; set; }
-        public bool SupportPlanReminders { get; set; }
-        public bool ObservationLogReminders { get; set; }
-        public bool EventReminders { get; set; }
-        public bool ContentReminders { get; set; }
-        public bool AdvocacyAlerts { get; set; }
-        public bool PrivateMessageAlerts { get; set; }
-        public bool NotificationsDigest { get; set; }
-
-        public bool Subscribed_DailyDigest { get; set; }
-        public bool Subscribed_WeeklyDigest { get; set; }
-
-        public virtual ICollection<Quiz> CompletedQuizes {get; set;}
-
-
-        //new member properties keep popping up. 
-        /* In case of emergency implement operation key/value them all
-        private Dictionary<string, string> _extendedMemberProperties;
-        public Dictionary<string, string> ExtendedMemberProperties
+        public static string GetMemberPublicProfile(string ScreenName)
         {
-            get
-            {
-                return _extendedMemberProperties;
-            }
-            set 
-            {
-                _extendedMemberProperties = value; 
-            }
+            return string.Format(MainsectionItem.GetHomePageItem()
+                .GetMyAccountFolder()
+                .GetPublicAccountFolder()
+                .GetPublicAccountPage()
+                .GetUrl() + "?{0}={1}", 
+                Constants.ACCOUNT_EMAIL, ScreenName);
         }
-        */
 
+        public static string GetMemberPublicProfile(this Member Member)
+        {
+            return GetMemberPublicProfile(Member.ScreenName);
+        }
     }
 }
---
+++ UnderstoodDotOrg.Domain/Membership/MemberModelExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Domain.Understood.Quiz;
+
+namespace UnderstoodDotOrg.Domain.Membership
+{
+    public partial class Member
+    {
+        //private bool _dataIsDirty = false;
+
+        /// <summary>
+        /// bg: added in to track our Unauthorized Member flag.
+        // .Net Membership already has a Comment column in the db.
+        /// </summary>
+        public string Comment { get; set; }
+
+        /// <summary>
+        /// bg: I wanted to keep the email address inside the member instead of passing it around as a parameter
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// bg: 
+        /// </summary>
+        public string Password { get; set; }
+
+        /// <summary>
+        //    bg: sitecore GUID for english/spanish/etc.
+        //    currently only being set when a user subscribes for a personalized newsletter
+        /// </summary>
+        public Guid PreferedLanguage { get; set; }
+        public bool AgreedToSignUpTerms { get; set; }
+        public string MobilePhoneNumber { get; set; }
+        public bool SupportPlanReminders { get; set; }
+        public bool ObservationLogReminders { get; set; }
+        public bool EventReminders { get; set; }
+        public bool ContentReminders { get; set; }
+        public bool AdvocacyAlerts { get; set; }
+        public bool PrivateMessageAlerts { get; set; }
+        public bool NotificationsDigest { get; set; }
+
+        public bool Subscribed_DailyDigest { get; set; }
+        public bool Subscribed_WeeklyDigest { get; set; }
+
+        public virtual ICollection<Quiz> CompletedQuizes { get; set; }
+
+
+        //new member properties keep popping up. 
+        /* In case of emergency implement operation key/value them all
+        private Dictionary<string, string> _extendedMemberProperties;
+        public Dictionary<string, string> ExtendedMemberProperties
+        {
+            get
+            {
+                return _extendedMemberProperties;
+            }
+            set 
+            {
+                _extendedMemberProperties = value; 
+            }
+        }
+        */
+
+    }
+}
