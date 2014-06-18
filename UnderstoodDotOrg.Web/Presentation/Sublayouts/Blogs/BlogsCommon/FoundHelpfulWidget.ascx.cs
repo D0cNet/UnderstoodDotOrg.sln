406cdc6f9fde9aba871157361f7bd722d4443910
zarnold <zarnold@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-18 22:04:06 +0000

    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1396 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Blogs/BlogsCommon/FoundHelpfulWidget.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Blogs/BlogsCommon/FoundHelpfulWidget.ascx.cs
@@ -9,8 +9,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using UnderstoodDotOrg.Common;
+using UnderstoodDotOrg.Domain.Membership;
 using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.CommunityTemplates.Blogs;
 using UnderstoodDotOrg.Domain.TelligentCommunity;
+using UnderstoodDotOrg.Domain.Understood.Activity;
 using UnderstoodDotOrg.Framework.UI;
 
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Blogs.BlogsCommon
@@ -21,80 +23,107 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Blogs.BlogsCommon
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            var contentId = blogCig.ContentId.Raw;
+            Guid ContentId = new Guid(blogCig.ContentId.Raw);
+            Guid MemberId = this.CurrentMember.MemberId;
+            ActivityLog log = new ActivityLog();
 
-            LikeCount.Text = CommunityHelper.GetTotalLikes(contentId).ToString();
-
+            bool washelpful = log.FoundItemHelpful(ContentId, MemberId);
+            int likeCount = log.GetActivityCountByValue(ContentId, Constants.UserActivity_Values.FoundHelpful_True);
+            string commentCount = CommunityHelper.ReadComments(blogCig.BlogId, blogCig.BlogPostId).Count.ToString();
+ 
+            //to get if the content was NOT helpful
+            //bool wasNOThelpful = log.FoundItemNotHelpful(ContentId, MemberId);
+            //LikeCount.Text = CommunityHelper.GetTotalLikes(ContentId.ToString()).ToString();
             var blogId = blogCig.BlogId.Raw;
             var blogPostId = blogCig.BlogPostId.Raw;
-            CommentCount.Text = CommunityHelper.ReadComments(blogId, blogPostId).Count.ToString();
+            CommentCount.Text = commentCount;
+            LikeCount.Text = likeCount.ToString();
             var blogPostInfo = CommunityHelper.ReadBlogBody(Int32.Parse(blogId), Int32.Parse(blogPostId));
             btnLike.CommandArgument = btnUnlike.CommandArgument = blogPostInfo.ContentId + "&" + blogPostInfo.ContentTypeId;
         }
         protected void btnThisHelped_Click(object sender, EventArgs e)
-        {
+        {         
+            ActivityLog log = new ActivityLog();
             LinkButton btn = (LinkButton)(sender);
             string ids = btn.CommandArgument;
             string[] s = ids.Split('&');
+            Guid ContentId = new Guid(s[0]);
+            //string contentTypeId = s[1];
+            Guid MemberId = this.CurrentMember.MemberId;
+            
+            if(!(log.FoundItemHelpful(ContentId, MemberId)))
+            {
+                // instantiate MM
+                MembershipManager mgr = new MembershipManager();
+                // write to the DB
+                mgr.LogMemberHelpfulVote(MemberId, ContentId, Constants.UserActivity_Values.FoundHelpful_True, Constants.UserActivity_Types.FoundHelpfulVote);
+                Response.Redirect(Request.RawUrl);
+            }
 
-            string contentId = s[0];
-            string contentTypeId = s[1];
+            //var webClient = new WebClient();
 
-            var webClient = new WebClient();
+            //// replace the "admin" and "Admin's API key" with your valid user and apikey!
+            //var adminKey = String.Format("{0}:{1}", Settings.GetSetting(Constants.Settings.TelligentAdminApiKey), "admin");
+            //var adminKeyBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
 
-            // replace the "admin" and "Admin's API key" with your valid user and apikey!
-            var adminKey = String.Format("{0}:{1}", Settings.GetSetting(Constants.Settings.TelligentAdminApiKey), "admin");
-            var adminKeyBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
+            //webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
+            //webClient.Headers.Add("Rest-Impersonate-User", this.CurrentMember.ScreenName.Trim());
+            //var requestUrl = String.Format("{0}api.ashx/v2/likes.xml", Settings.GetSetting(Constants.Settings.TelligentConfig));
 
-            webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
-            webClient.Headers.Add("Rest-Impersonate-User", this.CurrentMember.ScreenName.Trim());
-            var requestUrl = String.Format("{0}api.ashx/v2/likes.xml", Settings.GetSetting(Constants.Settings.TelligentConfig));
+            //var values = new NameValueCollection();
+            //values.Add("ContentId", contentId);
+            //values.Add("ContentTypeId", contentTypeId);
 
-            var values = new NameValueCollection();
-            values.Add("ContentId", contentId);
-            values.Add("ContentTypeId", contentTypeId);
+            //var xml = Encoding.UTF8.GetString(webClient.UploadValues(requestUrl, values));
 
-            var xml = Encoding.UTF8.GetString(webClient.UploadValues(requestUrl, values));
-
-            Console.WriteLine(xml);
+            //Console.WriteLine(xml);
         }
         protected void btnDidntHelp_Click(object sender, EventArgs e)
         {
+            ActivityLog log = new ActivityLog();
             LinkButton btn = (LinkButton)(sender);
             string ids = btn.CommandArgument;
             string[] s = ids.Split('&');
+            Guid ContentId = new Guid(s[0]);
+            //string contentTypeId = s[1];
+            Guid MemberId = this.CurrentMember.MemberId;
 
-            string contentId = s[0];
-            string contentTypeId = s[1];
-
-            var username = this.CurrentMember.ScreenName.Trim();
-            var like = new LikeModel();
-            if (!string.IsNullOrEmpty(username))
+            if ((log.FoundItemHelpful(ContentId, MemberId)))
             {
-                like = CommunityHelper.GetLike(this.CurrentMember.ScreenName.Trim(), contentId, contentTypeId);
+                // instantiate MM
+                MembershipManager mgr = new MembershipManager();
+                // write to the DB
+                mgr.LogMemberHelpfulVote(MemberId, ContentId, Constants.UserActivity_Values.FoundHelpful_False, Constants.UserActivity_Types.FoundHelpfulVote);
+                Response.Redirect(Request.RawUrl);
             }
-            using (var webClient = new WebClient())
-            {
-                try
-                {
-                    // replace the "admin" and "Admin's API key" with your valid user and apikey!
-                    var adminKey = String.Format("{0}:{1}", Settings.GetSetting(Constants.Settings.TelligentAdminApiKey), "admin");
-                    var adminKeyBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
 
-                    webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
-                    webClient.Headers.Add("Rest-Method", "DELETE");
+            //var like = new LikeModel();
+            //if (!string.IsNullOrEmpty(username))
+            //{
+            //    like = CommunityHelper.GetLike(this.CurrentMember.ScreenName.Trim(), contentId, contentTypeId);
+            //}
+            //using (var webClient = new WebClient())
+            //{
+            //    try
+            //    {
+            //        // replace the "admin" and "Admin's API key" with your valid user and apikey!
+            //        var adminKey = String.Format("{0}:{1}", Settings.GetSetting(Constants.Settings.TelligentAdminApiKey), "admin");
+            //        var adminKeyBase64 = Convert.ToBase64String(Encoding.UTF8.GetBytes(adminKey));
 
-                    var requestUrl = String.Format("{0}api.ashx/v2/likes.xml", Settings.GetSetting(Constants.Settings.TelligentConfig));
+            //        webClient.Headers.Add("Rest-User-Token", adminKeyBase64);
+            //        webClient.Headers.Add("Rest-Method", "DELETE");
 
-                    var values = new NameValueCollection();
-                    values.Add("ContentUrl", like.ContentUrl);
+            //        var requestUrl = String.Format("{0}api.ashx/v2/likes.xml", Settings.GetSetting(Constants.Settings.TelligentConfig));
 
-                    var xml = Encoding.UTF8.GetString(webClient.UploadValues(requestUrl, values));
+            //        var values = new NameValueCollection();
+            //        values.Add("ContentUrl", like.ContentUrl);
 
-                    Console.WriteLine(xml);
-                }
-                catch { } //TODO: Add logging
-            }
+            //        var xml = Encoding.UTF8.GetString(webClient.UploadValues(requestUrl, values));
+
+            //        Console.WriteLine(xml);
+            //    }
+            //    catch { } //TODO: Add logging
+            //}
         }
     }
 }
