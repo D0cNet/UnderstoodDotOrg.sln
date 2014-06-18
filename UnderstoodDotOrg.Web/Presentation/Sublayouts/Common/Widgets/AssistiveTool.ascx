42654688af65afb282f95d0f10da44e2d5c08ed0
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-18 00:36:59 +0000

    Wired up dropdowns and content fields for Assistive Tool widget
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1377 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/Widgets/AssistiveTool.ascx
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/Widgets/AssistiveTool.ascx
@@ -1,1 +1,50 @@
 ﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="AssistiveTool.ascx.cs" Inherits="UnderstoodDotOrg.Web.Presentation.Sublayouts.Common.Widgets.AssistiveTool" %>
+<%@ Register TagPrefix="sc" Namespace="Sitecore.Web.UI.WebControls" Assembly="Sitecore.Kernel" %>
+
+ <header>
+    <h3><sc:FieldRenderer ID="frWidgetTitle" runat="server" FieldName="Widget Title" /></h3>
+    <sc:FieldRenderer ID="frWidgetCopy" runat="server" FieldName="Widget Copy" />
+  </header>
+
+  <div class="form select-container">
+
+    <fieldset>
+
+        <asp:Label runat="server" AssociatedControlID="ddlIssues" CssClass="visuallyhidden rs_skip"><%= UnderstoodDotOrg.Common.DictionaryConstants.SelectBehaviorLabel %></asp:Label>
+        <asp:DropDownList ID="ddlIssues" runat="server" CssClass="issue-select" />
+
+        <asp:Label  runat="server" AssociatedControlID="ddlGrades" CssClass="visuallyhidden rs_skip"><%= UnderstoodDotOrg.Common.DictionaryConstants.SelectGradeLabel %></asp:Label>
+        <asp:DropDownList ID="ddlGrades" runat="server"  CssClass="grade-select" />
+
+        <asp:Label runat="server" AssociatedControlID="ddlTechTypes" CssClass="visuallyhidden rs_skip"></asp:Label>
+        <asp:DropDownList ID="ddlTechTypes" runat="server" CssClass="tech-type-select" />
+
+        <input type="hidden" id="hfSelectedPlatform" class="hfSelectedPlatform" runat="server" />
+        <asp:Repeater ID="rptrDynPlatformDropdowns" runat="server">
+            <ItemTemplate>
+                <select data-type-id="<%# Eval("TypeId") %>" class="platform-select" style="display:none;">
+                    <option value="">Select Platform</option>
+                    <asp:Repeater ID="rptrPlatformOptions" runat="server" 
+                        ItemType="UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ToolsPages.AssisitiveToolsPages.ReviewData.AssistiveToolsPlatformItem">
+                        <ItemTemplate>
+                            <option value="<%# Item.ID.ToString() %>"><%# Item.Metadata.ContentTitle %></option>
+                        </ItemTemplate>
+                    </asp:Repeater>
+                </select>
+            </ItemTemplate>
+        </asp:Repeater>
+
+    </fieldset>
+
+    <div class="submit-button-wrap">
+      <asp:Button runat="server" ID="btnSubmit" CssClass="button" />
+    </div>
+
+  </div><!-- .form -->
+
+  <footer class="powered-by">
+    <h5><sc:FieldRenderer ID="frWidgetFooterHeading" runat="server" FieldName="Widget Footer Heading" /></h5>
+    <div class="logo">
+      <sc:Image runat="server" ID="imgFooterLogo" Field="Widget Footer Logo" />
+    </div>
+  </footer>
--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/Widgets/AssistiveTool.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/Widgets/AssistiveTool.ascx.cs
@@ -1,19 +1,129 @@
-﻿using System;
+﻿using Sitecore.Data.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Folders.LearningTool;
 using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Shared.Widgets;
 using UnderstoodDotOrg.Framework.UI;
+using UnderstoodDotOrg.Common.Extensions;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ToolsPages.AssisitiveToolsPages.ReviewData;
+using UnderstoodDotOrg.Common.Comparers;
 
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Common.Widgets
 {
     public partial class AssistiveTool : BaseSublayout<AssistiveToolWidgetItem>
     {
+        private static string AssistiveToolsGlobalsFolderId = "{493EB983-FDE9-46E4-85C8-EE45EABFE91B}";
+        private Item AssistiveToolsGlobalsFolder { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            BindContent();
 
+            if (!IsPostBack)
+            {
+                BindControls();
+            }
+        }
+
+        private void BindContent()
+        {
+            btnSubmit.Text = Model.ToolWidget.WidgetButtonText.Rendered;
+
+            frWidgetCopy.Item = frWidgetTitle.Item = frWidgetFooterHeading.Item
+                = imgFooterLogo.Item = Model;
+        }
+
+        private void BindControls()
+        {
+            // TODO: refactor so code can be re-used with landing page
+            AssistiveToolsGlobalsFolder = Sitecore.Context.Database.GetItem(AssistiveToolsGlobalsFolderId);
+
+            var issuesFolder = (AssistiveToolsIssueFolderItem)AssistiveToolsGlobalsFolder.Children
+                .FirstOrDefault(i => i.IsOfType(AssistiveToolsIssueFolderItem.TemplateId));
+            if (issuesFolder != null)
+            {
+                var issues = issuesFolder.GetIssues()
+                    .Select(issue => new
+                    {
+                        Text = issue.Metadata.ContentTitle.Rendered,
+                        Value = issue.ID.ToString()
+                    });
+                ddlIssues.DataSource = issues;
+                ddlIssues.DataTextField = "Text";
+                ddlIssues.DataValueField = "Value";
+                ddlIssues.DataBind();
+                ddlIssues.Items.Insert(0, new ListItem("Select Issue", string.Empty));
+            }
+
+            var gradesFolder = (AssistiveToolsGradesFolderItem)AssistiveToolsGlobalsFolder.Children
+                .FirstOrDefault(i => i.IsOfType(AssistiveToolsGradesFolderItem.TemplateId));
+            if (gradesFolder != null)
+            {
+                var grades = gradesFolder.GetGradeRanges()
+                    .Select(grade => new
+                    {
+                        Text = grade.Metadata.ContentTitle.Rendered,
+                        Value = grade.ID.ToString()
+                    });
+                ddlGrades.DataSource = grades;
+                ddlGrades.DataTextField = "Text";
+                ddlGrades.DataValueField = "Value";
+                ddlGrades.DataBind();
+                ddlGrades.Items.Insert(0, new ListItem("Select Grade", string.Empty));
+            }
+
+            var typesFolder = (AssistiveToolsTypeFolderItem)AssistiveToolsGlobalsFolder.Children
+                .FirstOrDefault(i => i.IsOfType(AssistiveToolsTypeFolderItem.TemplateId));
+            if (typesFolder != null)
+            {
+                var techTypes = typesFolder.GetTechTypes()
+                    .Select(grade => new
+                    {
+                        Text = grade.Metadata.ContentTitle.Rendered,
+                        Value = grade.ID.ToString()
+                    });
+                ddlTechTypes.DataSource = techTypes;
+                ddlTechTypes.DataTextField = "Text";
+                ddlTechTypes.DataValueField = "Value";
+                ddlTechTypes.DataBind();
+                ddlTechTypes.Items.Insert(0, new ListItem("Select Technology", string.Empty));
+            }
+
+            var platformsFolder = (AssistiveToolsPlatformFolderItem)AssistiveToolsGlobalsFolder.Children
+                .FirstOrDefault(i => i.IsOfType(AssistiveToolsPlatformFolderItem.TemplateId));
+            if (platformsFolder != null)
+            {
+                var platforms = platformsFolder.GetPlatforms()
+                    .Where(i => i.IsOfType(AssistiveToolsPlatformItem.TemplateId))
+                    .Select(i => (AssistiveToolsPlatformItem)i)
+                    .Where(platform => platform.CorrespondingType.Item != null)
+                    .GroupBy(platform => platform.CorrespondingType.Item, new BaseItemComparer())
+                    .Select(group => new
+                    {
+                        TypeId = group.Key.ID.ToString(),
+                        Platforms = group.AsEnumerable()
+                    });
+
+                rptrDynPlatformDropdowns.DataSource = platforms;
+                rptrDynPlatformDropdowns.ItemDataBound += rptrDynPlatformDropdowns_ItemDataBound;
+                rptrDynPlatformDropdowns.DataBind();
+            }
+        }
+
+        void rptrDynPlatformDropdowns_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.IsItem())
+            {
+                var platforms = DataBinder.Eval(e.Item.DataItem, "Platforms") as IEnumerable<AssistiveToolsPlatformItem>;
+                var rptrPlatformOptions = e.FindControlAs<Repeater>("rptrPlatformOptions");
+
+                rptrPlatformOptions.DataSource = platforms;
+                rptrPlatformOptions.DataBind();
+            }
         }
     }
 }
