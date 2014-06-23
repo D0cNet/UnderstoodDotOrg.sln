d72c7f12afc2edb934bf3677473963c08463bb97
zkniebel <zkniebel@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-23 20:11:30 +0000

    Updated assets from DP
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1459 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Includes/js/advocacy.js
+++ UnderstoodDotOrg.Web/Presentation/Includes/js/advocacy.js
@@ -2,79 +2,94 @@
  * Definition for the TakeAction javascript module.
  */
 
-(function($){
+(function ($) {
 
-  // Initialize the module on page load.
-  $(document).ready(function() {
-    new U.TakeAction();
-  });
+    // Initialize the module on page load.
+    $(document).ready(function () {
+        new U.TakeAction();
+    });
 
-  U.TakeAction = function() {
-    var self = this;
+    U.TakeAction = function () {
+        var self = this;
 
-    // Handle moving sidebar find-this-helpful module around depending on window width
-    var $module = $('.take-action-module');
-    // if module exists on the page
-    if(!$module.length) { return; }
+        // Handle moving sidebar find-this-helpful module around depending on window width
+        var $module = $('.take-action-module');
+        // if module exists on the page
+        if (!$module.length) { return; }
 
-    var $findHelpfulLarge = $('.take-action-large');
-    var $findHelpfulSmall = $('.take-action-small');
+        var $findHelpfulLarge = $('.take-action-large');
+        var $findHelpfulSmall = $('.take-action-small');
 
-    detect();
-    jQuery(window).resize(detect);
+        detect();
+        jQuery(window).resize(detect);
 
-    function detect(){
-      // only above 650 viewport or nonresponsive
-      if(Modernizr.mq('(min-width: 650px)') || !Modernizr.mq('only all')){
-        $module.appendTo($findHelpfulLarge);
-      } else {
-        $module.appendTo($findHelpfulSmall);
-      }
-    }
+        function detect() {
+            // only above 650 viewport or nonresponsive
+            if (Modernizr.mq('(min-width: 650px)') || !Modernizr.mq('only all')) {
+                $module.appendTo($findHelpfulLarge);
+                jQuery('.l-take-action-share').prependTo($findHelpfulLarge);
+            } else {
+                $module.appendTo($findHelpfulSmall);
+                jQuery('.l-take-action-share').appendTo('.page-topic .row');
+            }
+        }
 
-    return this;
-  };
+        return this;
+    };
 
 })(jQuery);
 /**
  * Definition for the AdvocacyArticle javascript module.
  */
 
-(function($){
+(function ($) {
 
-  // Initialize the module on page load.
-  $(document).ready(function() {
-    new U.AdvocacyArticle();
-  });
+    // Initialize the module on page load.
+    $(document).ready(function () {
+        new U.AdvocacyArticle();
+    });
 
-  U.AdvocacyArticle = function() {
-    var self = this;
+    U.AdvocacyArticle = function () {
+        var self = this;
 
-    if( jQuery('.l-advocacy-article').length !== 0){
-      // Handle moving share and save module around depending on window width
-      var $module = $('.share-save-container');
-      // if module exists on the page
-      if(!$module.length) { return; }
+        if (jQuery('.l-advocacy-article').length !== 0) {
+            // Handle moving share and save module around depending on window width
+            var $module = $('.share-save-container');
+            // if module exists on the page
+            if (!$module.length) { return; }
 
-      var $ShareSavePagetopic = $('.share-save-pagetopic');
-      var $ShareSaveInline = $('.share-save-inline');
+            var $ShareSavePagetopic = $('.share-save-pagetopic');
+            var $ShareSaveInline = $('.share-save-inline');
 
-      detect();
-      jQuery(window).resize(detect);
+            detect();
+            jQuery(window).resize(detect);
 
-      function detect(){
-        // only above 650 viewport or nonresponsive
-        if(Modernizr.mq('(min-width: 650px)') || !Modernizr.mq('only all')){
-          $module.appendTo($ShareSaveInline);
-        } else {
-          $module.appendTo($ShareSavePagetopic );
+            function detect() {
+                // only above 650 viewport or nonresponsive
+                if (Modernizr.mq('(min-width: 650px)') || !Modernizr.mq('only all')) {
+                    $module.appendTo($ShareSaveInline);
+                } else {
+                    $module.appendTo($ShareSavePagetopic);
+                }
+            }
         }
-      }
-    }
 
-    return this;
-  };
+        return this;
+    };
 
 })(jQuery);
+(function ($) {
+
+    $(document).ready(function () {
+        var uniformElements = [
+          '.selector',
+          '.radio'
+        ];
+
+        new U.Global.readspeakerUniform(uniformElements);
+    });
+
+})(jQuery);
+
 
 
