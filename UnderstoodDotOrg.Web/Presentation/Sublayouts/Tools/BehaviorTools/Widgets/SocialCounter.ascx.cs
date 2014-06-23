befce904ab95963e603a74d625dcbaca023f4f52
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-23 21:38:16 +0000

    Wired up helpful count to behavior tools
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1466 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/BehaviorTools/Widgets/SocialCounter.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/BehaviorTools/Widgets/SocialCounter.ascx.cs
@@ -4,13 +4,16 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UnderstoodDotOrg.Common;
 using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Base.BasePageItems;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ToolsPages.BehaviorToolsPages;
 using UnderstoodDotOrg.Domain.TelligentCommunity;
+using UnderstoodDotOrg.Domain.Understood.Activity;
 using UnderstoodDotOrg.Framework.UI;
 
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.BehaviorTools.Widgets
 {
-    public partial class SocialCounter : BaseSublayout
+    public partial class SocialCounter : BaseSublayout<BehaviorToolsAdvicePageItem>
     {
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -20,9 +23,17 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.BehaviorTools.Widge
         private void BindContent()
         {
             BehaviorAdvicePageItem item = Sitecore.Context.Item;
+            ActivityLog log = new ActivityLog();
 
             litCommentsCount.Text = CommunityHelper.GetTotalComments(item.BlogId.Raw, item.BlogPostId.Raw).ToString();
-            litHelpfulCount.Text = CommunityHelper.GetTotalLikes(item.ContentId.Raw).ToString();
+            
+            Guid contentId;
+            int helpfulCount = 0;
+            if (Guid.TryParse(Model.BehaviorAdvicePage.ContentId.Raw, out contentId))
+            {
+                helpfulCount = log.GetActivityCountByValue(contentId, Constants.UserActivity_Values.FoundHelpful_True);
+            }
+            litHelpfulCount.Text = helpfulCount.ToString();
         }
     }
 }
