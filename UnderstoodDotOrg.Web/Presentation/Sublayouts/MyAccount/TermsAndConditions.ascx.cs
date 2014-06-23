b0df979dee93a06675d0db746be7c0bc0074fbef
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-06-23 20:23:23 +0000

    undoing Karega's redirect
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1460 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/MyAccount/TermsAndConditions.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/MyAccount/TermsAndConditions.ascx.cs
@@ -49,7 +49,7 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.MyAccount
             }
             
             //redirect based on interrupt
-            this.ReturnRedirect();
+            //this.ReturnRedirect();
 
             //if no sign-up interrupt was set, redirect as normal
             Response.Redirect(url);
