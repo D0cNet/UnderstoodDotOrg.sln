cfca70fc0a0e170ed304907f35d7dd3232410727
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-02 20:07:59 +0000

    Guess I missed two files...
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1690 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/LocationTest.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using UnderstoodDotOrg.Services.LocationServices;
+
+namespace UnderstoodDotOrg.Web.Presentation
+{
+    public partial class LocationTest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            uxLocation.Text = GeoIPLookup.GetCountry("50.241.102.161");
+        }
+    }
+}
