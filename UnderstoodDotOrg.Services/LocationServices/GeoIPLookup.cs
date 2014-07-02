34c0445bdc6a711eb5aee6a1e9c712522fa2598e
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-02 20:06:42 +0000

    that one file I missed...
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1689 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Services/LocationServices/GeoIPLookup.cs
@@ -0,0 +1,36 @@
+﻿using MaxMind.GeoIP2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Common;
+
+namespace UnderstoodDotOrg.Services.LocationServices
+{
+    public static class GeoIPLookup
+    {
+
+
+        public static string GetCountry(string ClientIP)
+        {
+            // future licensed webservice call
+            //int _userId = int.Parse(Sitecore.Configuration.Settings.GetSetting(Constants.GeoIPLookup.GeoIPUserId));
+            //string _licenseKey = Sitecore.Configuration.Settings.GetSetting(Constants.GeoIPLookup.GeoIPLicenseKey);
+
+            //var client = new WebServiceClient(_userId, _licenseKey);
+            //var omni = client.Country(ClientIP);
+
+            //return omni.Country.IsoCode;
+
+            // temporary free database
+            string _database = Sitecore.Configuration.Settings.GetSetting(Constants.GeoIPLookup.GeoIPDatabaseName);
+            string _dataFolder = Sitecore.Configuration.Settings.DataFolder;
+
+            var reader = new DatabaseReader(_dataFolder + "\\" + _database);
+            var country = reader.Country(ClientIP);
+            
+            return country.Country.IsoCode;
+        }
+    }
+}
