0b8cb7d2a1ecbeee2e362e688dace21f4a279dba
bgarnier <bgarnier@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-07 19:22:37 +0000

    Script to delete old import data, delete old version of seed data, and insert new, good data
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1795 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Build/Packages/07072014/DeleteAndInsertForAssistiveTech.sql
@@ -0,0 +1,7 @@
+
+-- clear out seed data and previous import data
+DELETE FROM dbo.CSMUserReviews
+DELETE FROM dbo.CSMReviewsToSkills
+DELETE FROM dbo.CSMUserReviewSkills
+
+-- insert corrected seed skill data
