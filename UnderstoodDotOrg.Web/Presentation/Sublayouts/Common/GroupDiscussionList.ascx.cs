be2eafbdb418a01aa36daeb61c5af41f351e3b1e
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-11 16:46:42 +0000

    tweaks to formatting, removed commented code no longer needed
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1885 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/GroupDiscussionList.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/GroupDiscussionList.ascx.cs
@@ -1,42 +1,15 @@
-﻿using Sitecore.Data.Items;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
 using System.Web.UI.WebControls;
-using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.CommunityTemplates.GroupsTemplate;
-using UnderstoodDotOrg.Domain.TelligentCommunity;
-using UnderstoodDotOrg.Domain.Understood.Common;
 using UnderstoodDotOrg.Common.Extensions;
+using UnderstoodDotOrg.Domain.Understood.Common;
 using UnderstoodDotOrg.Framework.UI;
-using System.Web.UI.HtmlControls;
 using UnderstoodDotOrg.Web.Presentation.Sublayouts.Common.Cards;
+
 namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Common
 {
     public partial class GroupDiscussionList : BaseSublayout//System.Web.UI.UserControl
     {
-        public override void DataBind()
-        {
-            rptDiscussionList.DataBind();
-        }
-
-
-        protected void Page_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        //protected void rptChildCard_ItemDataBound(object sender, RepeaterItemEventArgs e)
-        //{
-
-        //    Repeater childIssues_repeater = (Repeater)e.Item.FindControl("rptChildIssues");
-        //    if (childIssues_repeater != null)
-        //    {
-        //        childIssues_repeater.DataSource = ((ChildCardModel)e.Item.DataItem).IssueList;
-        //        childIssues_repeater.DataBind();
-        //    }
-        //}
         protected void rptDiscussionList_ItemDataBound(object sender, RepeaterItemEventArgs e)
         {
             if (e.Item.DataItem is ReplyModel)
@@ -47,7 +20,6 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Common
                 if (ProfileCommentCard != null)
                 {
                     ProfileCommentCard.Member = item.Author;
-
                 }
 
                 ThanksButton btnThanks = e.FindControlAs<ThanksButton>("btnThanks");
@@ -62,31 +34,14 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Common
                             btnThanks.LoadState(item.Author.UserName);
                             btnThink.LoadState(item.Author.UserName);
                         }
-
                     }
-
-                    //if (btnThink != null)
-                    //{
-                    //    btnThink.LoadState(recipientScreenName);
-                    //}
                 }
-
-
-                //HtmlAnchor hrefName = e.FindControlAs<HtmlAnchor>("hrefName");
-                //if (hrefName != null)
-                //{
-                //    hrefName.HRef = ((ReplyModel)e.Item.DataItem).Author.ProfileLink;
-                //}
-
-
-                //Repeater childModel_repeater = (Repeater)e.Item.FindControl("rptChildCard");
-                //if (childModel_repeater != null)
-                //{
-                //    childModel_repeater.DataSource = ((ReplyModel)e.Item.DataItem).Author.Children;
-                //    childModel_repeater.DataBind();
-                //}
             }
+        }
 
+        public override void DataBind()
+        {
+            rptDiscussionList.DataBind();
         }
 
         public List<ReplyModel> DataSource
