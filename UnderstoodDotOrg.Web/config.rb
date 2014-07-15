2e68822a5b407994299411d1c49700b9d2602edc
jblecher <jblecher@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-15 22:19:42 +0000

    UNAO-584 add DP front-end toolchain config files
    see https://digitalpulp.atlassian.net/wiki/display/UN/Sitecore+Development+Environment for details.
    
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1935 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+require "breakpoint"
+
+# asset dirs
+css_dir = "Presentation/includes/css"
+sass_dir = "Presentation/includes/css.source"
+images_dir = "Presentation/includes/images"
+javascripts_dir = "Presentation/includes/js"
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
