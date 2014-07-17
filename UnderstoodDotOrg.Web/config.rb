4a7f72060c404b54b31646078b3d007b8c3918b8
jblecher <jblecher@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-17 18:49:49 +0000

    UNAO-584 revise compass cachebuster method
    this affects sprites and background images generated via compass.
    
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@1991 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/config.rb
+++ UnderstoodDotOrg.Web/config.rb
@@ -18,3 +18,40 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = true
+
+
+# override default compass cache buster (appears to be file mtime- or 
+# ctime-based) with more deterministic hashing via SHA2. shorten the 
+# hash to 8 chars to reduce ugliness.
+require 'digest'
+
+asset_cache_buster do |path, real_path|
+  if File.exists?(real_path)
+	cachebuster = Digest::SHA2.file(real_path).hexdigest[0..7]
+
+    {:path => path, :query => cachebuster}
+  end
+end
+
+
+
+# # Make a copy of sprites with a name that has no uniqueness of the hash.
+# on_sprite_saved do |filename|
+# 	if File.exists?(filename)
+# 		FileUtils.cp filename, filename.gsub(%r{-s[a-z0-9]{10}\.png$}, '.png')
+# 		# Note: Compass outputs both with and without random hash images.
+# 		# To not keep the one with hash, add: (Thanks to RaphaelDDL for this)
+# 		FileUtils.rm_rf(filename)
+# 	end
+# end
+
+# # Replace in stylesheets generated references to sprites
+# # by their counterparts without the hash uniqueness.
+# on_stylesheet_saved do |filename|
+# 	if File.exists?(filename)
+# 		css = File.read filename
+# 		File.open(filename, 'w+') do |f|
+# 			f << css.gsub(%r{(?<start>-s)(?<hash>[a-z0-9]{10})(?<file>\.png)}, '.png?v=\k<hash>')
+# 		end
+# 	end
+# end
