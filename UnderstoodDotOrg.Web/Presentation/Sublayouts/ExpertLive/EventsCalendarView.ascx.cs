9c227ab86adca24fcf31225bcc397a9e95a3ca58
avelez <avelez@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-26 15:52:25 +0000

    PFF-443 - adding calendar control first pass
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2305 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/ExpertLive/EventsCalendarView.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.ExpertLive
+{
+    public partial class EventsCalendarView : System.Web.UI.UserControl
+    {
+
+        private void Page_Load(object sender, EventArgs e)
+        {
+            // Put user code to initialize the page here
+        }
+
+    }
+}
