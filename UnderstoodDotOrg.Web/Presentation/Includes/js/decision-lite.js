e039447467a1b7c944ced4c10270d61fd3886dc0
zkniebel <zkniebel@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-28 14:14:40 +0000

    Completed UNAO-713: Restored JS accidentally removed during previous commit when files were updated from DP
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2339 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Includes/js/decision-lite.js
+++ UnderstoodDotOrg.Web/Presentation/Includes/js/decision-lite.js
@@ -2,6 +2,20 @@
 
   $(document).ready(function() {
     new U.decisionQuiz();
+
+      var $hfAnswerIndex = $(".hfAnswerIndex");
+      var $nextQuestion = $(".next-question");
+
+      var $answerWrapper = $(".answer-wrapper");
+      var $answerControl = $answerWrapper.find("button, input[type='radio']");
+
+      $answerControl.on("click", function () {
+          var $this = $(this);
+          $hfAnswerIndex.val($this.attr("data-answer-index"));
+          $answerWrapper.hide();
+          $("." + $this.attr("data-indication-answer-id")).show();
+          $nextQuestion.show();
+      });
   });
 
   U.decisionQuiz = function() {
