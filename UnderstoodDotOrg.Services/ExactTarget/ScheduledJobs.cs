35e43e0d1dcf531a0d434aaee585d992d54df731
imccloy <imccloy@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-28 16:07:34 +0000

    Added skeleton for Scheduled Tasks
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2351 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Services/ExactTarget/ScheduledJobs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sitecore.Diagnostics;
+
+namespace UnderstoodDotOrg.Services.ExactTarget
+{
+    class ScheduledJobs
+    {
+        public void WeeklyGeneralNewsLetter()
+        {
+
+            Sitecore.Diagnostics.Log.Debug("Scheduled Task ran");
+
+        }
+
+
+
+
+    }
+}
