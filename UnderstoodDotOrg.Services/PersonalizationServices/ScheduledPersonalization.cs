61b0fa54942bf13261d6a279492b122c0390a225
bgarnier <bgarnier@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-07-29 16:33:14 +0000

    Added the code to services that will be called by sitecore task schedule to rerun personalization for every child in the system.
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2465 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Services/PersonalizationServices/ScheduledPersonalization.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Domain.Personalization;
+namespace UnderstoodDotOrg.Services.PersonalizationServices
+{
+
+
+    public static class ScheduledPersonalization
+    {
+
+        public static void RunPersonalizationUpdate()
+        {
+            Sitecore.Diagnostics.Log.Debug("Scheduled Daily Personaliztion Update Starting." + DateTime.Now.ToString());
+            try
+            {
+                string sql = "SELECT DISTINCT [ChildId] FROM [dbo].[PersonalizedContent] ";//GET all of the children in the system who already have personalized content
+
+                  using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["membership"].ConnectionString))
+                  {
+                      conn.Open();
+                      using (SqlCommand cmd = new SqlCommand(sql, conn))
+                      {
+                
+                          SqlDataReader reader = cmd.ExecuteReader();
+                          if (reader.HasRows)
+                          {
+                             Guid childId = new Guid();
+                              while (reader.Read())
+                              {
+                                  childId= reader.GetGuid(0);
+                                  PersonalizationHelper.RefreshAndSavePersonalizedContent(childId);
+                                  Sitecore.Diagnostics.Log.Debug("Finished Processing Personalization for Child: " + 
+                                      childId.ToString() + " [" + DateTime.Now.ToShortDateString() +"]");
+                              }
+                          }
+                      }
+                  }
+            }
+            catch (Exception ex)
+            {
+                Sitecore.Diagnostics.Log.Debug("**** Error thrown while refreshing personalized content. **** " + Environment.NewLine 
+                    + "Message: " +ex.Message + Environment.NewLine + 
+                        "Stack Trace:" + ex.StackTrace );
+        
+            }
+            Sitecore.Diagnostics.Log.Debug("Scheduled Daily Personaliztion Update Ending." + DateTime.Now.ToString());
+     
+        }
+    }
+}
