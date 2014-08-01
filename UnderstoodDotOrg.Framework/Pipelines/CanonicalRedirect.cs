3f7abe71df5d24d0637505bd7d3f9dc3be92402b
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-01 03:22:07 +0000

    added Sitecore Pipeline for canonical 301 redirects and associated configs
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2606 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Framework/Pipelines/CanonicalRedirect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Common.Extensions;
+
+namespace UnderstoodDotOrg.Framework.Pipelines
+{
+    public class CanonicalRedirect : Sitecore.Pipelines.HttpRequest.ItemResolver
+    {
+        public override void Process(Sitecore.Pipelines.HttpRequest.HttpRequestArgs args)
+        {
+            base.Process(args);
+
+            if (Sitecore.Context.Item != null && System.Web.HttpContext.Current != null)
+            {
+                var url = Sitecore.Context.Item.GetUrl();
+                var context = System.Web.HttpContext.Current;
+
+                if (!context.Request.RawUrl.Contains(url))
+                {
+                    context.Response.RedirectPermanent(url);
+                }
+            }
+        }
+    }
+}
