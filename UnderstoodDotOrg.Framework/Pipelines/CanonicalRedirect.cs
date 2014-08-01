a2c19ff1eb4e97c58436d9a2caf9a3ae746b837f
chaddad <chaddad@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-01 17:57:36 +0000

    ignore redirects if url includes Sitecore wildcard placeholder
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2641 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Framework/Pipelines/CanonicalRedirect.cs
+++ UnderstoodDotOrg.Framework/Pipelines/CanonicalRedirect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnderstoodDotOrg.Common;
 using UnderstoodDotOrg.Common.Extensions;
 
 namespace UnderstoodDotOrg.Framework.Pipelines
@@ -16,10 +17,16 @@ namespace UnderstoodDotOrg.Framework.Pipelines
             if (Sitecore.Context.Item != null && System.Web.HttpContext.Current != null)
             {
                 var url = Sitecore.Context.Item.GetUrl();
+                
                 var context = System.Web.HttpContext.Current;
+                var rawUrl = context.Request.RawUrl;
 
-                if (!context.Request.RawUrl.Contains(url) && !context.Request.RawUrl.Contains("/sitecore/"))
+                if (!rawUrl.Contains(url) && !rawUrl.Contains("/sitecore/") && !url.Contains(Sitecore.Configuration.Settings.GetSetting(Constants.Settings.WildcardUrlPlaceholder)))
                 {
+                    if (!string.IsNullOrEmpty(context.Request.Url.Query))
+                    {
+                        url += context.Request.Url.Query;
+                    }
                     context.Response.RedirectPermanent(url);
                 }
             }
