41f5bececdd4c7c31ecc22ab3f9daac442fb7b54
lchiao <lchiao@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-01 20:34:38 +0000

    UNAO-971 added missing class
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2644 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Domain/Search/AssistiveToolSearchResultSet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnderstoodDotOrg.Domain.SitecoreCIG.Poses.Pages.ToolsPages.AssisitiveToolsPages;
+
+namespace UnderstoodDotOrg.Domain.Search
+{
+    public class AssistiveToolSearchResultSet
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryTitle { get; set; }
+        public string HelpModalContent { get; set; }
+        public bool ShowHelpModal { get; set; }
+        public int TotalCount { get; set; }
+        public int DisplayCount { get; set; }
+        public IEnumerable<AssistiveToolsReviewPageItem> SearchResults { get; set; }
+        public bool HasMoreResults { get; set; }
+    }
+}
