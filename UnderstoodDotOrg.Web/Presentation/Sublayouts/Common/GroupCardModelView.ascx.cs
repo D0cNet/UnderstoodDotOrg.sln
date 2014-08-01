38b1ae450905a5b0359be720cf6fb49b1ec88ee0
dallegrone <dallegrone@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-01 14:22:12 +0000

    Fixing previous fix with null error
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2614 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/GroupCardModelView.ascx.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Common/GroupCardModelView.ascx.cs
@@ -52,6 +52,7 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Common
             var qs = HttpUtility.ParseQueryString(Request.QueryString.ToString());
             qs.Set("q", txtSearch.Text);
             qs.Set("g", Sitecore.Context.Item.ID.ToString());
+            qs.Set("b", "false");
             Response.Redirect(String.Format("{0}?{1}", LinkManager.GetItemUrl(Sitecore.Context.Database.GetItem("{97CEFC40-892F-4E44-B94F-26522EB1F7B3}")), qs));
         }
     }
