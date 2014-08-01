541b221746f5aa36b43f62695dd41510ff7ea3ea
tseifert <tseifert@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-01 15:32:19 +0000

    re-adding tyce-mobile-player.js
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2621 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

---
+++ UnderstoodDotOrg.Web/Presentation/Includes/js/tyce-mobile-player.js
@@ -0,0 +1,49 @@
+(function() {
+
+    // listeners for messages that come from the TYCE object
+    window.addEventListener('message', function(event){
+        if(event.data.funct == 'loadvideo'){
+            VP.loadVideo(event.data.id);
+        }
+    }, false);
+
+  // vars for experience, video player, experience module, and html player element
+    var experience,
+        VP,
+        modExp,
+        html_player;
+
+    // player ready callback - register necessary event listeners for playback authorization, and create the overlay
+    function onPlayerReady(evt) {
+        html_player = document.getElementById('player');
+    VP.addEventListener(brightcove.api.events.MediaEvent.COMPLETE, onMediaComplete);
+    // VP.addEventListener(brightcove.api.events.MediaEvent.STOP, onMediaStop);
+    // VP.addEventListener(brightcove.api.events.MediaEvent.PLAY, onMediaPlay);
+    // VP.addEventListener(brightcove.api.events.MediaEvent.BEGIN, onMediaBegin);
+    // VP.addEventListener(brightcove.api.events.MediaEvent.CHANGE, onMediaChange);
+    }
+
+    function onMediaComplete(evt) {
+        parent.postMessage('mediaComplete', '*');
+    }
+
+  // TODO : add fullscreen 
+  // media change event - need to re-authorize the user
+  // function onMediaChange(evt) { }
+  // function onMediaStop(evt) {
+  //   parent.postMessage('mediaStop', '*');
+  // }
+
+    // Register the plugin and get necessary modules
+    experience = brightcove.api.getExperience();
+    VP = experience.getModule(brightcove.api.modules.APIModules.VIDEO_PLAYER);
+    modExp = experience.getModule(brightcove.api.modules.APIModules.EXPERIENCE);
+    
+    // Continue if ready, or wait for ready event
+    if (modExp.getReady()) {
+        onPlayerReady();
+    } else {
+        modExp.addEventListener(brightcove.player.events.ExperienceEvent.TEMPLATE_READY, onPlayerReady);
+    }
+
+}());
