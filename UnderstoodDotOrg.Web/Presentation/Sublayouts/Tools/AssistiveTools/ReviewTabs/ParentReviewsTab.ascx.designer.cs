19b057c0f9fc60504ba034b7e243ad61e6858667
jlane <jlane@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-04 17:52:37 +0000

    UNAO-1244 hide review box if there are no reviews AT
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2672 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/ReviewTabs/ParentReviewsTab.ascx.designer.cs
+++ UnderstoodDotOrg.Web/Presentation/Sublayouts/Tools/AssistiveTools/ReviewTabs/ParentReviewsTab.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace UnderstoodDotOrg.Web.Presentation.Sublayouts.Tools.AssistiveTools.Revi
     public partial class ParentReviewsTab {
         
         /// <summary>
+        /// reviewDetailsBox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl reviewDetailsBox;
+        
+        /// <summary>
         /// litAverageRating control.
         /// </summary>
         /// <remarks>
