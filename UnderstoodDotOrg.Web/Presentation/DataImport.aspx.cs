35466e7e0bec8f9d69ae72dfc93c43e6a22491eb
jlane <jlane@7f4e098a-8ec1-c442-913e-02cc3c41ae5c>
2014-08-04 16:11:24 +0000

    bug fix for data import
    git-svn-id: https://svn.otpartners.com/svn/Poses/trunk@2664 7f4e098a-8ec1-c442-913e-02cc3c41ae5c

--- UnderstoodDotOrg.Web/Presentation/DataImport.aspx.cs
+++ UnderstoodDotOrg.Web/Presentation/DataImport.aspx.cs
@@ -33,9 +33,9 @@ namespace UnderstoodDotOrg.Web.Presentation
             {
                 try
                 {
-                    XmlTextReader apps = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=app&special_needs=1delta=1406563200");
-                    XmlTextReader games = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=game&special_needs=1delta=1406563200");
-                    XmlTextReader websites = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=website&special_needs=1delta=1406563200");
+                    XmlTextReader apps = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=app&special_needs=1&delta=1406563200");
+                    XmlTextReader games = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=game&special_needs=1&delta=1406563200");
+                    XmlTextReader websites = new XmlTextReader("http://api.commonsensemedia.org/api/v2/reviews/browse?api_key=534823b372928738c93803b534a7a770&channel=website&special_needs=1&delta=1406563200");
 
                     totalEntries += ImportCategory(GetEntries(apps));
                     totalEntries += ImportCategory(GetEntries(games));
